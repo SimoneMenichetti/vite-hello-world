11ce81118ea964d1bcab9f74218e56bb2ffc3c65
simone menichetti <simon3menichettui95@gmail.com>
2024-07-04 17:07:45 +0200

    build: add bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,22 @@
+<script>
+// qui va il codice Javascript
+export default{
+    name:"AppHero",
+}
+</script>
+
+<template>
+    <!-- qui html -->
+    <div>
+        <img src="../assets/vue.svg" alt="vue img">
+    </div>
+</template>
+
+<style scoped>
+/* qui css */
+
+ img{
+    width: 50%;
+    height: 50%;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+// qui va il codice Javascript
+export default{
+    name:"AppTitle",
+}
+</script>
+
+<template>
+    <!-- qui html -->
+    <h1> “La mia prima app con Vite!”</h1>
+
+    <div></div>
+</template>
+
+<style scoped>
+/* qui css */
+h1{
+    color:grey;
+}
+</style>
